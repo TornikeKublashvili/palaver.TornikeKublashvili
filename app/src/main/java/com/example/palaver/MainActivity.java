c2a64ea933babf932f05afdd456554930ccfef7a
Guenesesin <gunes_2009@hotmail.de>
2020-05-10 15:03:40 +0200

    splash-screen

--- app/src/main/java/com/example/palaver/MainActivity.java
+++ app/src/main/java/com/example/palaver/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.palaver;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
+  //  Handler handler = new Handler();
     public static SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         sharedPreferences = getSharedPreferences("MySharedPreferences", MODE_PRIVATE);
         if (sharedPreferences.getBoolean("IsLoggedIn", false)) {
